00e14d004a41aef5b81fb90ac376be99af49659d
ywang <ywang@inf.ethz.ch>
2019-07-25 10:55:18 -0700

    fix generatePolygon randomness

--- pytorch_points/utils/geometry_utils.py
+++ pytorch_points/utils/geometry_utils.py
@@ -1,4 +1,4 @@
-import math, random
+import numpy as np
 
 def generatePolygon( ctrX, ctrY, aveRadius, irregularity, spikeyness, numVerts) :
     '''Start with the centre of the polygon at ctrX, ctrY, 
@@ -16,39 +16,34 @@ def generatePolygon( ctrX, ctrY, aveRadius, irregularity, spikeyness, numVerts) 
     Returns a list of vertices, in CCW order.
     '''
 
-    irregularity = clip( irregularity, 0,1 ) * 2*math.pi / numVerts
-    spikeyness = clip( spikeyness, 0,1 ) * aveRadius
+    irregularity = np.clip( irregularity, 0,1 ) * 2*np.pi / numVerts
+    spikeyness = np.clip( spikeyness, 0,1 ) * aveRadius
 
     # generate n angle steps
     angleSteps = []
-    lower = (2*math.pi / numVerts) - irregularity
-    upper = (2*math.pi / numVerts) + irregularity
+    lower = (2*np.pi / numVerts) - irregularity
+    upper = (2*np.pi / numVerts) + irregularity
     sum = 0
     for i in range(numVerts) :
-        tmp = random.uniform(lower, upper)
+        tmp = np.random.uniform(lower, upper)
         angleSteps.append( tmp )
         sum = sum + tmp
 
     # normalize the steps so that point 0 and point n+1 are the same
-    k = sum / (2*math.pi)
+    k = sum / (2*np.pi)
     for i in range(numVerts) :
         angleSteps[i] = angleSteps[i] / k
 
     # now generate the points
     points = []
-    angle = random.uniform(0, 2*math.pi)
+    # angle = random.uniform(0, 2*np.pi)
+    angle = 0
     for i in range(numVerts) :
-        r_i = clip( random.gauss(aveRadius, spikeyness), 0, 2*aveRadius )
-        x = ctrX + r_i*math.cos(angle)
-        y = ctrY + r_i*math.sin(angle)
+        r_i = np.clip(np.random.normal(aveRadius, spikeyness), 0, 2*aveRadius )
+        x = ctrX + r_i*np.cos(angle)
+        y = ctrY + r_i*np.sin(angle)
         points.append((x,y))
 
         angle = angle + angleSteps[i]
 
     return points
-
-def clip(x, min, max):
-    if( min > max ) :  return x    
-    elif( x < min ) :  return min
-    elif( x > max ) :  return max
-    else :             return x
