970f03b7f09d6859bf4d31692fc219a44f278ad7
ywang <yifan.wang@inf.ethz.ch>
2020-03-05 18:38:02 +0100

    pytorch 1.4.0 use data_ptr

--- pytorch_points/_ext/interpolate.cpp
+++ pytorch_points/_ext/interpolate.cpp
@@ -14,10 +14,10 @@ extern THCState *state;
 
 void three_nn_wrapper_fast(int b, int n, int m, at::Tensor unknown_tensor,
     at::Tensor known_tensor, at::Tensor dist2_tensor, at::Tensor idx_tensor) {
-    const float *unknown = unknown_tensor.data<float>();
-    const float *known = known_tensor.data<float>();
-    float *dist2 = dist2_tensor.data<float>();
-    int *idx = idx_tensor.data<int>();
+    const float *unknown = unknown_tensor.data_ptr<float>();
+    const float *known = known_tensor.data_ptr<float>();
+    float *dist2 = dist2_tensor.data_ptr<float>();
+    int *idx = idx_tensor.data_ptr<int>();
 
     cudaStream_t stream = THCState_getCurrentStream(state);
     three_nn_kernel_launcher_fast(b, n, m, unknown, known, dist2, idx, stream);
@@ -30,10 +30,10 @@ void three_interpolate_wrapper_fast(int b, int c, int m, int n,
                          at::Tensor weight_tensor,
                          at::Tensor out_tensor) {
 
-    const float *points = points_tensor.data<float>();
-    const float *weight = weight_tensor.data<float>();
-    float *out = out_tensor.data<float>();
-    const int *idx = idx_tensor.data<int>();
+    const float *points = points_tensor.data_ptr<float>();
+    const float *weight = weight_tensor.data_ptr<float>();
+    float *out = out_tensor.data_ptr<float>();
+    const int *idx = idx_tensor.data_ptr<int>();
 
     cudaStream_t stream = THCState_getCurrentStream(state);
     three_interpolate_kernel_launcher_fast(b, c, m, n, points, idx, weight, out, stream);
@@ -45,10 +45,10 @@ void three_interpolate_grad_wrapper_fast(int b, int c, int n, int m,
                             at::Tensor weight_tensor,
                             at::Tensor grad_points_tensor) {
 
-    const float *grad_out = grad_out_tensor.data<float>();
-    const float *weight = weight_tensor.data<float>();
-    float *grad_points = grad_points_tensor.data<float>();
-    const int *idx = idx_tensor.data<int>();
+    const float *grad_out = grad_out_tensor.data_ptr<float>();
+    const float *weight = weight_tensor.data_ptr<float>();
+    float *grad_points = grad_points_tensor.data_ptr<float>();
+    const int *idx = idx_tensor.data_ptr<int>();
 
     cudaStream_t stream = THCState_getCurrentStream(state);
     three_interpolate_grad_kernel_launcher_fast(b, c, n, m, grad_out, idx, weight, grad_points, stream);
